4f12aaf30ea5203125e90c3b3695f11dfed8d580
majidpawar9 <majidpawar9@gmail.com>
2023-11-14 17:35:45 -0500

    Initial Commit

---
+++ WeatherWise-iOS-Group-4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherWise-iOS-Group-4
+//
+//  Created by Majid Pawar on 2023-11-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
